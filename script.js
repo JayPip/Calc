ccd8b8e7e3debb277bb1bae55cb6fd211de00329
JayPip <46865973+JayPip@users.noreply.github.com>
2019-12-28 00:50:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+function sum(){
+  var x=document.getElementById("sum1").value;
+  x = parseFloat(x);
+
+ var y=document.getElementById("sum2").value;
+y = parseFloat(y);
+var z= x + y ;
+
+
+
+alert(z);
+
+
+return;
+
+
+}
+
+
+
+function dif(){
+  var x=document.getElementById("dif1").value;
+    x = parseFloat(x);
+
+ var y=document.getElementById("dif2").value;
+    y = parseFloat(y);
+
+
+
+var z= x - y ;
+
+alert(z);
+
+
+return;
+
+
+}
